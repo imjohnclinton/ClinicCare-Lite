a233a5b0e2a836ea5e6dfb1a15cc95d0e39e3885
imjohnclinton <johnclintonkora@gmail.com>
2026-08-03 00:28:37 +0000

    Remove message model and add email handler utility

--- models/message.py
+++ models/message.py
@@ -1,14 +1,1 @@
-import smtplib
-from email.mime.text import MIMEText
- 
-def send_email(sender_email, sender_password, recipient_email, subject, body):
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = sender_email
-    msg['To'] = recipient_email
- 
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.starttls()
-        server.login(sender_email, sender_password)
-        server.send_message(msg)
 
