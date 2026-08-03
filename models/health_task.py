5b97c7f4490b43621893b0a40e3720fb2738ef42
imjohnclinton <johnclintonkora@gmail.com>
2026-08-03 00:16:01 +0000

    Add TaskSubmission model and JSON structure and modified HealthTask model

--- models/health_task.py
+++ models/health_task.py
@@ -1,25 +1,22 @@
 import json
-
+from datetime import datetime
+ 
 class HealthTask:
-    def __init__(self, task_id, title, description, clinician_id, due_date):
+    def __init__(self, task_id, title, description, due_date, clinic_id):
         self.task_id = task_id
         self.title = title
         self.description = description
-        self.clinician_id = clinician_id
         self.due_date = due_date
-
-    @staticmethod
-    def validate_title(title):
-        return len(title.strip()) > 0
-
+        self.clinic_id = clinic_id
+ 
     def save(self):
         with open('data/health_tasks.json', 'r+') as f:
             data = json.load(f)
             data[self.task_id] = {
                 'title': self.title,
                 'description': self.description,
-                'clinician_id': self.clinician_id,
-                'due_date': self.due_date
+                'due_date': self.due_date,
+                'clinic_id': self.clinic_id
             }
             f.seek(0)
             f.truncate()
